9cd6d0cc904604463b563e485884d1ac53514cb3
Ronnie Lutalo <ronnielutaro@gmail.com>
2022-10-03 15:18:37 +0300

    Update templates

---
+++ src/templates/Post.tsx
@@ -0,0 +1,48 @@
+import * as React from 'react';
+import { graphql, PageProps } from 'gatsby';
+import { PostTemplateQuery } from './__generated__/PostTemplateQuery';
+import PostScreen from '../components/screens/PostScreen';
+
+interface BlogPostProps extends PageProps {
+  data: PostTemplateQuery,
+}
+
+export const query = graphql`
+  query PostTemplateQuery ($slug: String!) {
+    mdx(fields: { slug: { eq: $slug } }) {
+      id
+      timeToRead
+      body
+      fields {
+        slug
+      }
+      frontmatter {
+        title
+        summary
+        date(formatString: "DD MMMM, YYYY")
+        cover {
+          childImageSharp {
+            fluid(
+              fit: COVER,
+              cropFocus: CENTER,
+              grayscale: false,
+              quality: 90,
+              maxWidth: 1000
+            ) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+      }
+    }
+  }
+`;
+
+const BlogPost = (props: BlogPostProps): React.ReactElement => {
+  const { data } = props;
+  return (
+    <PostScreen post={data} />
+  );
+};
+
+export default BlogPost;

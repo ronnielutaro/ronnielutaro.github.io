196087b78659fae39134ae396643e667c91f7c59
Ronnie Lutalo <ronnielutaro@gmail.com>
2022-10-03 15:12:45 +0300

    Update configuration

---
+++ gatsby-node.ts
@@ -0,0 +1,75 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { createFilePath } from 'gatsby-source-filesystem';
+import { CreateNodeArgs, CreatePagesArgs } from 'gatsby';
+import * as path from 'path';
+import { routes } from './src/constants/routes';
+import { projectMapToArray } from './src/utils/project';
+import { projects } from './src/data/projects';
+import { Project } from './src/types/Project';
+
+export function onCreateNode(args: CreateNodeArgs): void {
+  // Create a slug field for markdown post nodes.
+  const { actions, node, getNode } = args;
+  const { createNodeField } = actions;
+  if (node.internal.type === 'Mdx') {
+    const slug = createFilePath({ node, getNode });
+    createNodeField({
+      node,
+      name: 'slug',
+      value: routes.blog.path + slug,
+    });
+  }
+}
+
+type CreatePostPagesQuery = {
+  data?: {
+    allMdx?: {
+      nodes?: any[],
+    },
+  },
+};
+
+async function createPostPages(args: CreatePagesArgs): Promise<void> {
+  const { actions, graphql } = args;
+  const { createPage } = actions;
+  const result: CreatePostPagesQuery = await graphql(`
+    query CreatePostPagesQuery {
+      allMdx {
+        nodes {
+          fields {
+            slug
+          }
+        }
+      }
+    }
+  `);
+  (result?.data?.allMdx?.nodes || []).forEach((node) => {
+    createPage({
+      path: node.fields.slug,
+      component: path.resolve('./src/templates/Post.tsx'),
+      context: {
+        // Data passed to context is available in page queries as GraphQL variables.
+        slug: node.fields.slug,
+      },
+    });
+  });
+}
+
+async function createProjectPages(args: CreatePagesArgs): Promise<void> {
+  const { actions } = args;
+  const { createPage } = actions;
+  projectMapToArray(projects).forEach((project: Project) => {
+    createPage({
+      path: `${routes.projects.path}/${project.id}`,
+      component: path.resolve('./src/templates/Project.tsx'),
+      context: {
+        projectID: project.id,
+      },
+    });
+  });
+}
+
+export async function createPages(args: CreatePagesArgs): Promise<void> {
+  await createPostPages(args);
+  await createProjectPages(args);
+}

d03b33d8c0c8dfc15c0ffab29c5105e1c1032341
Ronnie Lutalo <ronnielutaro@gmail.com>
2022-11-26 15:22:29 +0300

    Update site meta-data & favicon

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -5,25 +5,27 @@ import utilStyles from '../../styles/utils.module.css'
 import Link from 'next/link'
 
 const name = 'Ronnie Lutalo'
-export const siteTitle = 'Next.js Sample Website'
+export const siteTitle = 'Ronnie Lutalo'
+
+export const siteDescription = `Software Engineer | CG Artist | XR (VR) developer from Kampala, Uganda. 
+Ronnie Lutalo writes about new & recent stuff he is working on while sharing lessons learnt along the way.
+From Product development, Software Applications, Extended Reality (Virtual Reality), Game Design (Unity-3D 
+& C#), Web design & development, 2D/3D Computer Graphics & Animation, to contributing to the Open Source Community.`;
+
+// Path to the image that is placed in the 'public/images' folder.
+export const siteImage = '/images/site-meta-image-01.png';
 
 export default function Layout({ children, home }) {
   return (
     <div className={styles.container}>
       <Head>
         <link rel="icon" href="/favicon.ico" />
-        <meta
-          name="description"
-          content="Learn how to build a personal website using Next.js"
-        />
-        <meta
-          property="og:image"
-          content={`https://og-image.vercel.app/${encodeURI(
-            siteTitle
-          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.zeit.co%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
-        />
         <meta name="og:title" content={siteTitle} />
         <meta name="twitter:card" content="summary_large_image" />
+        <meta name="description" content={siteDescription} key="desc" />
+        <meta property="og:title" content={siteTitle} />
+        <meta property="og:description" content={siteDescription} />
+        <meta property="og:image" content={siteImage} />
       </Head>
       <header className={styles.header}>
         {home ? (

a818523512b73e5dbd7e36446855765652e9bc9f
Ronnie Lutalo <ronnielutaro@gmail.com>
2022-12-01 15:50:49 +0300

    Update _app component

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,39 @@
-import '../styles/global.css'
+import React, { useEffect, useState } from "react";
+import '../styles/plugins.css';
+import '../styles/style.css';
+import '../styles/templete.css';
+import '../styles/skin/skin-1.css';
+import '../plugins/slick/slick.min.css';
+import '../plugins/slick/slick-theme.min.css';
+import 'react-modal-video/css/modal-video.min.css';
+import { useScrollPosition } from '@n8tb1t/use-scroll-position'
+import Layout from "../components/Layout/Layout";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }) {
+    {/* const [body_, setbody_] = useState();
+    const [header, setHeader] = useState("fixed");
+    const [header_, setHeader_] = useState();
+    let scrollPosition = useScrollPosition();
+
+    useEffect(() => {
+      setbody_(document.querySelector("body"));
+      setHeader_(document.getElementsByClassName("main-bar-wraper"));
+      }, []);
+
+
+    var element = document.getElementById("fix-header"); 	
+      
+    if(typeof(element) != 'undefined' && element != null){
+      header === "fixed" && scrollPosition > 10
+        ? header_ && header_[0].classList.add("is-fixed")
+        : header_ && header_[0].classList.remove("is-fixed"); 	
+    }
+  */}
+  return (
+    <Layout>
+      <Component {...pageProps} />;
+    </Layout>
+  );
 }
+
+export default MyApp;

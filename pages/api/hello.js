cca13d261101aae2da1f37d9863bac642ecb1278
Ronnie Lutalo <ronnielutaro@gmail.com>
2022-12-01 15:51:05 +0300

    Update Home page

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,3 +1,7 @@
-export default (req, res) => {
-  res.status(200).json({ text: 'Hello' })
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+export default async function handler(req, res) {
+  res.status(200).json({ success: "success" });
+
+  client.close();
 }
--- pages/index.js
+++ pages/index.js
@@ -1,58 +1,84 @@
-import Head from 'next/head'
-import Layout, { siteTitle } from '../components/Layout/Layout'
-import utilStyles from '../styles/utils.module.css'
-import { getSortedPostsData } from '../lib/posts'
-import Link from 'next/link'
-import Date from '../components/date'
+import React,{Component} from 'react';
+import HomeOwlSlider from '../components/UI/HomeOwlSlider';
+import HomeOwlSlider2 from '../components/UI/HomeOwlSlider2';
+import ImgCarouselContent from '../components/UI/ImgCarouselContent';
+import HomeTab from '../components/UI/HomeTab';
+import Counter from '../components/UI/Counter';
+import TestimonialCarousel from '../components/UI/TestimonialCarousel';
+import FormStyle from '../components/UI/FormStyle';
 
-export default function Home({ allPostsData }) {
-  return (
-    <Layout home>
-      <Head>
-        <title>{siteTitle}</title>
-      </Head>
-      <section className={utilStyles.headingMd}>
-        <p>
-          Hello world! I&apos;m <strong>Ronnie</strong>, a
-          Software Engineer and Visual Artist
-          from Kampala, Uganda. Driven by desire and curiosity for creating Technology and
-          Experiences that unlock new possibilities.
-          Over time, I&apos;ve worked on various projects using a wide range
-          of Tools & Technologies like C#, ASP.NET Core,
-          Entity Framework Core,
-          JavaScript/TypeScript, React.js/Next.js,
-          Microsoft Azure (Cloud), WPF, SQL.
-        </p>
-        <p>
-          When I&apos;m not coding, you can find me blogging here,
-          or working on Visual Arts.
-          I&apos;m always excited to talk about Technology and Visual Arts so feel free to reach out.
-          The best way to reach me is through email (ronnielutaro@gmail.com).
-        </p>
-      </section>
-      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}>Blog</h2>
-        <ul className={utilStyles.list}>
-          {allPostsData.map(({ id, date, title }) => (
-            <li className={utilStyles.listItem} key={id}>
-              <Link href={`/posts/${id}`}>{title}</Link>
-              <br />
-              <small className={utilStyles.lightText}>
-                <Date dateString={date} />
-              </small>
-            </li>
-          ))}
-        </ul>
-      </section>
-    </Layout>
-  )
+class index extends Component{
+	render(){
+		return(
+            <div className="page-wraper">
+                <div className="page-content bg-white">
+                    {/*  Slider Banner */}
+                    <div className="owl-slider-banner main-slider">
+                        <HomeOwlSlider />
+                    </div>
+                    {/*  Slider Banner */}
+                    <div className="content-block">
+                        {/* <!-- What I Do Start--> */}
+                        <div className="section-full bg-white content-inner-1 about-us">
+                            <div className="container">
+                                <div className="row ">
+                                    <div className="col-lg-7 col-md-8">
+                                        <div className="abuot-box row" >
+                                            <div className="col-lg-4">
+                                                <h2 className="box-title m-tb0">What I Do?<span className="bg-primary"></span></h2>
+                                                <h4 className="text-gray-dark">Well...</h4>
+                                            </div>
+                                            <div className="col-lg-8">
+                                                <p>
+                                                    There&apos;s actually much more to me than just a few fancy titles that don&apos;t 
+                                                    really mean anything, hahah...
+                                                    Well, over time I&apos;ve worked on various projects involving development of Technology, 
+                                                    mainly Software Systems. 
+                                                    I mostly code using C# but also work with a few other Tools like ASP.NET Core, EF Core,
+                                                    WPF, JavaScript/TypeScript, React.js(Next.js), Unity 3D Engine.
+                                                    When I&apos;m not coding, you can find me blogging or working
+                                                    on Visual Arts (2D/3D Computer Graphics).
+                                                </p>
+                                                <p>
+                                                    If you&apos;re looking to explore some of my recent work, then you&apos;re in the right place.
+                                                </p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="col-lg-5 col-md-4 about-img" >
+                                        <picture>
+                                            <img src="/images/about/pic4.jpg" data-tilt alt="" />
+                                        </picture>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        {/* <!-- What I Do End--> */}
+                        {/* <!-- Testimonials --> */}
+                        <div className="section-full content-inner" style={{ backgroundImage: "/images/background/bg-map.jpg", backgroundPosition: "center", backgroundRepeat: "no-repeat" }}>
+                            <div className="container">
+                                <div className="section-head text-center">
+                                    <h2 className="box-title m-tb0">What It&apos;s Like Working With Ronnie?<span className="bg-primary"></span></h2>
+                                    <h4 className="text-gray-dark">Here&apos;s what others say...</h4>
+                                </div>
+                                <div className="section-content m-b30 ">
+                                    <TestimonialCarousel />
+                                </div>
+                            </div>
+                        </div>
+                        {/* <!-- Testimonials END --> */}
+                        
+                        {/* <!-- Get in touch --> */}
+                        <div className="section-full overlay-primary-dark bg-img-fix" style={{ backgroundImage: "/images/background/bg1.jpg" }}>
+                            <FormStyle />
+                        </div>
+                        {/* <!-- Get in touch --> */}
+                        
+                    </div>
+                </div>	
+                
+            </div>	
+		)
+	}
 }
-
-export async function getStaticProps() {
-  const allPostsData = getSortedPostsData()
-  return {
-    props: {
-      allPostsData
-    }
-  }
-}
+export default index;

83c4b75e4c1b924733483f28707625e882992355
Ronnie Lutalo <ronnielutaro@gmail.com>
2022-12-10 21:54:23 +0300

    Fix Footer & remove stale links (closes #57)

--- components/Layout/Footer.js
+++ components/Layout/Footer.js
@@ -1,5 +1,6 @@
 import Link from 'next/link';
 import React, { Component } from 'react';
+import { profile } from '../../data/profile';
 
 const date = new Date();
 const year = date.getFullYear();
@@ -16,24 +17,19 @@ class Footer extends Component {
                                     <div className="widget widget_services border-0">
                                         <h5 className="m-b30 text-white">Work</h5>
                                         <ul>
-											<li><Link href="#">Backend </Link></li>
-                                            <li><Link href="#">Web Design </Link></li>
-                                            <li><Link href="#">Product Design</Link></li>
-                                            <li><Link href="#">Digital Design</Link></li>
-                                            <li><Link href="#">3D Modelling</Link></li>
-                                            <li><Link href="#">Virtual Reality</Link></li>
-                                            <li><Link href="#">Concept Art</Link></li>
+											<li><Link href="#">Engineering </Link></li>
+                                            <li><Link href="#">Design </Link></li>
                                         </ul>
                                     </div>
                                 </div>
                                 <div className="col-7 col-lg-2 col-md-6 col-sm-6 footer-col-4">
                                     <div className="widget widget_services border-0">
-                                        <h5 className="m-b30 text-white">Useful Link</h5>
+                                        <h5 className="m-b30 text-white">Useful Links</h5>
                                         <ul>
-                                            <li><Link href="/about">About Me</Link></li>
-                                            <li><Link href="/blog/engineering">Engineering Blog </Link></li>
-                                            <li><Link href="#">Visual Arts Blog</Link></li>
-                                            <li><Link href="/">Home</Link></li>
+                                            <li><Link href="/about">About Me </Link></li>
+                                            <li><Link href="/blog">Blog </Link></li>
+                                            <li><Link href="/">Home </Link></li>
+                                            <li><Link href="/contact">Contact </Link></li>
                                         </ul>
                                     </div>
                                 </div>
@@ -50,7 +46,7 @@ class Footer extends Component {
                                     <div className="widget">
                                         <h5 className="m-b30 text-white">Subscribe To Newsletter</h5>
                                             <p className="text-capitalize m-b20">
-                                                Subscribe to newsletter and be the first to know whenever I share new content.
+                                                Subscribe to my newsletter and be the first to know whenever I release new content.
                                             </p>
                                         <div className="subscribe-form m-b20">
                                             <form className="dzSubscribe" action="script/mailchamp.php" method="post">
@@ -64,11 +60,11 @@ class Footer extends Component {
                                             </form>
                                         </div>
                                         <ul className="list-inline m-a0">
-                                            <li><a href="https://www.facebook.com/RLutalo/" target="_blank" className="site-button facebook circle mr-1" rel="noreferrer"><i className="fa fa-facebook" rel="noreferrer"></i></a></li>
-                                            <li><a href="https://www.linkedin.com/in/ronnie-lutalo-b73240aa/" target="_blank" className="site-button linkedin circle mr-1" rel="noreferrer"><i className="fa fa-linkedin"></i></a></li>
-                                            <li><a href="https://www.instagram.com/r_lutalo/" target="_blank" className="site-button instagram circle mr-1" rel="noreferrer"><i className="fa fa-instagram" rel="noreferrer"></i></a></li>
-                                            <li><a href="https://twitter.com/r_lutalo" target="_blank" className="site-button twitter circle mr-1" rel="noreferrer"><i className="fa fa-twitter"></i></a></li>
-                                            <li><a href="https://github.com/RonnieLutalo" target="_blank" className="site-button github circle mr-1" rel="noreferrer"><i className="fa fa-github"></i></a></li>
+                                            <a href={profile.socialLinks.Twitter} target="_blank" className="site-button twitter circle mr-1" rel="noreferrer"><i className="fa fa-twitter"></i></a>
+                                            <a href={profile.socialLinks.LinkedIn} target="_blank" className="site-button linkedin circle mr-1" rel="noreferrer"><i className="fa fa-linkedin"></i></a>
+                                            <a href={profile.socialLinks.GitHub} target="_blank" className="site-button github circle mr-1" rel="noreferrer"><i className="fa fa-github"></i></a>
+                                            <a href={profile.socialLinks.Instagram} target="_blank" className="site-button instagram circle mr-1" rel="noreferrer"><i className="fa fa-instagram" rel="noreferrer"></i></a>
+                                            <a href={profile.socialLinks.Facebook} target="_blank" className="site-button facebook circle mr-1" rel="noreferrer"><i className="fa fa-facebook" rel="noreferrer"></i></a>
                                         </ul>
                                     </div>
                                 </div>

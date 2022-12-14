624d70912428c3e2074906a61c75b7c01f217747
Ronnie Lutalo <ronnielutaro@gmail.com>
2022-12-14 10:55:44 +0300

    Fix linting

--- pages/blog.jsx
+++ pages/blog.jsx
@@ -38,49 +38,49 @@ export default function blog({ allPostsData }) {
         <ul>
             {allPostsData.map(({ id, date, title, summary, coverImage }) => (
                 <li key={id}>
-                <section className="project-text p-90-0-0">
-                <div id="anchor-1" className="container">
-                    <div className="row a-f">
-                        <div className="col-12 col-lg-2 text-center">
-                            {/* Author */}
-                            <div className="review">
-                                <div className="rv-cont">
-                                    <div className="avatar-sm">
-                                        {/* Author photo */}
-                                        <picture>
-                                            <img src="/images/people/pic1.jpg" alt="author" />
-                                        </picture>
+                    <section className="project-text p-90-0-0">
+                        <div id="anchor-1" className="container">
+                            <div className="row a-f">
+                                <div className="col-12 col-lg-2 text-center">
+                                    {/* Author */}
+                                    <div className="review">
+                                        <div className="rv-cont">
+                                            <div className="avatar-sm">
+                                                {/* Author photo */}
+                                                <picture>
+                                                    <img src="/images/people/pic1.jpg" alt="author" />
+                                                </picture>
+                                            </div>
+                                        </div>
+                                        <div className="rv-cont">
+                                            {/* Name */}
+                                            <p className="text-title mb-10">by {profile.fullName}</p>
+                                        </div>
+                                    </div>
+                                    {/* Author end */}
+                                    {/* Date */}
+                                    <p className="light opas mb-60"><Date dateString={date} /></p>
+                                </div>
+                                <div className="col-12 col-lg-10">
+                                    {/* Publication  cover */}
+                                    <picture>
+                                        <img className="pub-cover mb-30" src={coverImage} alt="Publication cover" />
+                                    </picture>
+                                    {/* Title */}
+                                    <h2 className="bold mb-30">{title}</h2>
+                                    {/* Text */}
+                                    <p className="light opas mb-30">{summary}</p>
+                                    <hr className="mb-30" />
+                                    <div className="text-center text-md-left">
+                                        {/* Button */}
+                                        <Link href={`/blog/${id}`} className="button margin-sm-bottom button-1 new-page-in">Read more <i className="fas fa-chevron-right"></i></Link>
                                     </div>
                                 </div>
-                                <div className="rv-cont">
-                                    {/* Name */}
-                                    <p className="text-title mb-10">by {profile.fullName}</p>
-                                </div>
-                            </div>
-                            {/* Author end */}
-                            {/* Date */}
-                            <p className="light opas mb-60"><Date dateString={date} /></p>
-                        </div>
-                        <div className="col-12 col-lg-10">
-                            {/* Publication  cover */}
-                            <picture>
-                                <img className="pub-cover mb-30" src={coverImage} alt="Publication cover" />
-                            </picture>
-                            {/* Title */}
-                            <h2 className="bold mb-30">{title}</h2>
-                            {/* Text */}
-                            <p className="light opas mb-30">{summary}</p>
-                            <hr className="mb-30" />
-                            <div className="text-center text-md-left">
-                                {/* Button */}
-                                <Link href={`/blog/${id}`} className="button margin-sm-bottom button-1 new-page-in">Read more <i className="fas fa-chevron-right"></i></Link>
                             </div>
                         </div>
-                    </div>
-                </div>
-            </section>
-            </li>
-        ))}
+                    </section>
+                </li>
+            ))}
         </ul>
         {/* Publication end */}
     </>
